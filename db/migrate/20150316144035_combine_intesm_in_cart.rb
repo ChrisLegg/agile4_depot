83fda922aa1fd6165f27af625bc799251bb0bb51
Chris Legg <chris@chrislegg.net>
2015-03-16 23:30:03 +0000

    Add testing for ajax cart

--- db/migrate/20150316144035_combine_intesm_in_cart.rb
+++ db/migrate/20150316144035_combine_intesm_in_cart.rb
@@ -1,4 +1,4 @@
-class CombineItemsInCart < ActiveRecord::Migration
+class CombineIntesmInCart < ActiveRecord::Migration
 
   def up
     # replace multiple items for a single product in a cart with a single item
