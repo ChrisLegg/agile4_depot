38290d3fae4fa492c2ecce79045392af552c9145
Chris Legg <chris@chrislegg.net>
2015-03-17 22:16:28 +0000

    Add user login functionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_action :authorize, only: [:create, :update, :destroy]
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   
@@ -80,8 +81,7 @@ class CartsController < ApplicationController
     
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to store_url, notice: 'Invalid cart'
-      
-    end    
+      redirect_to store_url, notice: 'Invalid cart'      
+    end
 
 end
